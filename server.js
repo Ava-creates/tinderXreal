889cb77eaf2ee0a8f8358077141b00f0f26127db
yaya1721 <Sonyahyun.hs@gmail.com>
2022-10-14 23:21:56 -0400

    first set up with tinder API

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+require("dotenv").config();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const multer = require("multer");
+const path = require("path");
 const express = require('express');
 const app = express(); 
 const port = process.env.PORT || 8011;
@@ -9,3 +14,31 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 app.get('/express_backend', (req, res) => { 
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); 
 });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.static(path.join(__dirname, "public")));
+
+// create constants for the application.
+const constants = {
+  matchRequestStatus: {
+    pending: 0,
+    accepted: 1,
+    rejected: -1,
+  },
+};
+
+// config multers.
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/img");
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${file.fieldname}-${Date.now()}.jpg`);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+// create datbase connection
