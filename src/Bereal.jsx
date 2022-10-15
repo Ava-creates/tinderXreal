f623a71a31efe4dd1d7583bc93d7a5016096be7d
Ali Shehbaz <alishahbaz072@gmail.com>
2022-10-14 22:32:03 -0400

    make css follow react conventions

--- src/Bereal.jsx
+++ src/Bereal.jsx
@@ -5,8 +5,8 @@ import { useEffect } from 'react';
 export const Bereal = () => {
   return (
     <div>
-    <video id="webcam" autoplay playsinline width="640" height="480"></video>
-    <canvas id="canvas" class="d-none"></canvas>
+    <video id="webcam" autoPlay playsInline width="640" height="480"></video>
+    <canvas id="canvas" className="d-none"></canvas>
     <audio id="snapSound" src="audio/snap.wav" preload = "auto"></audio>
     <p>Hello World</p>
     <StartCamera></StartCamera>
@@ -19,7 +19,6 @@ function StartCamera(){
 
 
 useEffect(() => {
-    // setTimeout(activateLasers, 1500);
     takePicture();
   });
 
@@ -28,28 +27,17 @@ useEffect(() => {
 
 function takePicture(){
 
-        const webcamElement = document.getElementById('webcam');
+const webcamElement = document.getElementById('webcam');
 const canvasElement = document.getElementById('canvas');
 const snapSoundElement = document.getElementById('snapSound');
 const webcam = new Webcam(webcamElement, 'user', canvasElement, snapSoundElement);
     webcam.start()
    .then(result =>{
+    console.log(result)
       console.log("webcam started");
    })
    .catch(err => {
        console.log(err);
    });
 
-let picture = webcam.snap();
 }
-
-
-// webcam.start()
-//    .then(result =>{
-//       console.log("webcam started");
-//    })
-//    .catch(err => {
-//        console.log(err);
-//    });
-
-// let picture = webcam.snap();
