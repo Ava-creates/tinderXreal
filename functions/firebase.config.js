e5665dc0ae432170215a64e6b17402091afbd226
yaya1721 <Sonyahyun.hs@gmail.com>
2022-10-15 21:29:00 -0400

    dependency issue

---
+++ functions/firebase.config.js
@@ -0,0 +1,11 @@
+/**
+  Enter the configuration for your Firebase web app
+  module.exports = {
+  apiKey: ...,
+  authDomain: ...,
+  projectId: ...,
+  storageBucket: ...,
+  messagingSenderId: ...,
+  appId: ...,
+  measurementId: ...
+}; */
